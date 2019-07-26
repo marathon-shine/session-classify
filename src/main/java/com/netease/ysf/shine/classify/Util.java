f823632b62a38ce61609636135aab4a580443543
zhoujianghua <zhoujianghua@corp.netease.com>
2019-07-26 21:24:38 +0800

    从文件读取数据的方法

--- src/main/java/com/netease/ysf/shine/classify/Util.java
+++ src/main/java/com/netease/ysf/shine/classify/Util.java
@@ -1,9 +1,6 @@
 package com.netease.ysf.shine.classify;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
+import java.io.*;
 import java.util.Map;
 
 public class Util {
@@ -11,7 +8,26 @@ public class Util {
     public static void printStatistics(Map<Integer, Statistics> statisticsMap) {
     }
 
-//    public static DataLine readFromFile(BufferedReader file) {
-//        file.readLine();
-//    }
+    public static DataLine readFromFile(BufferedReader reader) throws IOException {
+        String line = reader.readLine();
+        return fromLine(line);
+    }
+
+    private static DataLine fromLine(String line) {
+        String[] splits = line.split(":");
+        if (splits.length != 2) {
+            return null;
+        }
+        int category = Integer.parseInt(splits[0]);
+        String[] vectorStrs = splits[1].substring(1, splits[1].length() - 1).split(",");
+        float[] vector = new float[vectorStrs.length];
+        for (int i = 0; i < vector.length; ++i) {
+            vector[i] = Float.parseFloat(vectorStrs[i]);
+        }
+
+        DataLine dataLine = new DataLine();
+        dataLine.setCategory(category);
+        dataLine.setVector(vector);
+        return dataLine;
+    }
 }
