2eac306480117e280180361ade4175f9bc6f9fa3
王奇 <wangqi02@corp.netease.com>
2019-07-27 11:11:50 +0800

    [Feature] 新增SessionType源文件统计

--- src/main/java/com/netease/ysf/shine/tokenzier/FileProcessor.java
+++ src/main/java/com/netease/ysf/shine/tokenzier/FileProcessor.java
@@ -8,16 +8,59 @@ import org.apache.commons.io.LineIterator;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.*;
 
 
 public class FileProcessor {
 
     public static void main(String[] args) throws IOException {
 //        mergeFile("mergedSegmentedFile.txt", "new_message_694916_parsed.txt", "new_message_694917_parsed.txt");
-        mergeClassificationTranFile("mergedClassificationTranFile.txt",
-                new Pair<>("694916", "new_message_694916_parsed.txt.simple.vec.txt"),
-                new Pair<>("694917", "new_message_694917_parsed.txt.simple.vec.txt")
-        );
+//        mergeClassificationTranFile("mergedClassificationTranFile.txt",
+//                new Pair<>("694916", "new_message_694916_parsed.txt.simple.vec.txt"),
+//                new Pair<>("694917", "new_message_694917_parsed.txt.simple.vec.txt")
+//        );
+
+        fileStatistics("2018101112AllSession.txt");
+    }
+
+    public static void fileStatistics(String inputFileName) throws IOException {
+        String spllitter = "#####";
+        File inputFile = new File(Constants.fileBase + inputFileName );
+        LineIterator it = FileUtils.lineIterator(inputFile, "UTF-8");
+        Map<String, Integer> counter = new HashMap<>();
+        try {
+            while (it.hasNext()) {
+                String line = it.nextLine();
+                if(line!=null && line.contains(spllitter)){
+                    String[] splitted = line.split(spllitter);
+                    String key = splitted[0];
+                    if(!counter.containsKey(key)) {
+                        counter.put(key, 0);
+                    }
+                    counter.put(key, counter.get(key)+1);
+                }
+            }
+        } finally {
+            it.close();
+        }
+
+        //这里将map.entrySet()转换成list
+        List<Map.Entry<String,Integer>> list = new ArrayList<Map.Entry<String,Integer>>(counter.entrySet());
+        //然后通过比较器来实现排序
+        Collections.sort(list, new Comparator< Map.Entry<String,Integer> >() {
+            //升序排序
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o1.getValue().compareTo(o2.getValue());
+            }
+
+        });
+
+        for(Map.Entry<String,Integer> mapping:list){
+            if( mapping.getValue() > 10000 ) {
+//                System.out.println(mapping.getKey() + ":" + mapping.getValue());
+                System.out.println(mapping.getKey());
+            }
+        }
     }
 
     public static void mergeClassificationTranFile(String outputFileName, Pair<String, String>... files) throws IOException {
